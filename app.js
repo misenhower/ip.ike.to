d23a87f414327258404b7600bd3639214147f067
Unknown <matt@isenhower.com>
2017-11-06 17:01:19 -0800

    Add Docker config

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+// Proxy
+app.set('trust proxy', 'uniquelocal');
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
